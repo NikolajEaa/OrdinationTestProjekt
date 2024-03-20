53341ba56a17280ed16f3c4be1e0166f17dea357
Smeaguuul <mikkelhoffmannp@gmail.com>
2024-03-20 10:14:40 +0100

    Færdig lavet

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -11,43 +11,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
 
+    Controller controller = Controller.getController();
+    private Patient patient;
+    private Laegemiddel laegemiddel;
+
     @Test
     void opretPNOrdination() {
-        Controller controller = Controller.getController();
-        Patient patient = controller.opretPatient("12","Bent",78);
-        Laegemiddel laegemiddel = controller.opretLaegemiddel("Acetylsalicylsyre",0.1,0.2,0.5,"Styk");
-        PN pn = controller.opretPNOrdination(LocalDate.of(2024,12,12),LocalDate.of(2024,12,24),patient,laegemiddel,2);
+        Patient patient = controller.opretPatient("12", "Bent", 78);
+        Laegemiddel laegemiddel = controller.opretLaegemiddel("Acetylsalicylsyre", 0.1, 0.2, 0.5, "Styk");
+        PN pn = controller.opretPNOrdination(LocalDate.of(2024, 12, 12), LocalDate.of(2024, 12, 24), patient, laegemiddel, 2);
 
         assertNotNull(pn);
 
         LocalDate actualStartDato = pn.getStartDen();
-        LocalDate exptectedStartDato = LocalDate.of(2024,12,12);
-        assertEquals(exptectedStartDato,actualStartDato);
+        LocalDate exptectedStartDato = LocalDate.of(2024, 12, 12);
+        assertEquals(exptectedStartDato, actualStartDato);
 
         LocalDate actualSlutDato = pn.getSlutDen();
-        LocalDate exptectedSlutDato = LocalDate.of(2024,12,24);
-        assertEquals(exptectedSlutDato,actualSlutDato);
+        LocalDate exptectedSlutDato = LocalDate.of(2024, 12, 24);
+        assertEquals(exptectedSlutDato, actualSlutDato);
 
         PN actualPn = (PN) patient.getOrdinationer().get(0);
         PN expectedPN = pn;
-        assertEquals(expectedPN,actualPn);
+        assertEquals(expectedPN, actualPn);
 
         Laegemiddel actualLaegemiddel = pn.getLaegemiddel();
         Laegemiddel expectedLaemiddel = laegemiddel;
-        assertEquals(expectedLaemiddel,actualLaegemiddel);
+        assertEquals(expectedLaemiddel, actualLaegemiddel);
     }
 
     @Test
     void opretPNOrdinationSlutFørStart() {
         Controller controller = Controller.getController();
-        Patient patient = controller.opretPatient("12","Bent",78);
-        Laegemiddel laegemiddel = controller.opretLaegemiddel("Acetylsalicylsyre",0.1,0.2,0.5,"Styk");
-        PN pn = controller.opretPNOrdination(LocalDate.of(2024,12,24),LocalDate.of(2024,12,22),patient,laegemiddel,2);
+        Patient patient = controller.opretPatient("12", "Bent", 78);
+        Laegemiddel laegemiddel = controller.opretLaegemiddel("Acetylsalicylsyre", 0.1, 0.2, 0.5, "Styk");
+        PN pn = controller.opretPNOrdination(LocalDate.of(2024, 12, 24), LocalDate.of(2024, 12, 22), patient, laegemiddel, 2);
 
-
-    Controller controller = Controller.getController();
-    Patient patient;
-    Laegemiddel laegemiddel;
+    }
 
     @BeforeEach
     void setUp() {
@@ -55,10 +55,6 @@ class ControllerTest {
         laegemiddel = new Laegemiddel("Acetylsalicylsyre", 0.1, 0.2, 0.5, "Styk");
     }
 
-    @Test
-    void opretPNOrdination() {
-
-    }
 
     @Test
     void DagligFastGyldig_TC1() {
@@ -75,26 +71,26 @@ class ControllerTest {
 
         assertTrue(patient.getOrdinationer().contains(dagligFast));
     }
+
     @Test
     void DagligFastUgyldig_TC1() {
 
         DagligFast dagligFast = null;
         try {
-        dagligFast = controller.opretDagligFastOrdination(LocalDate.of(2024, 12, 12), LocalDate.of(2024, 12, 24), patient, laegemiddel, 0, 0, 0, 0);
-        }
-        catch (IllegalArgumentException e) {
+            dagligFast = controller.opretDagligFastOrdination(LocalDate.of(2024, 12, 12), LocalDate.of(2024, 12, 24), patient, laegemiddel, 0, 0, 0, 0);
+        } catch (IllegalArgumentException e) {
             assertTrue(e.getMessage().contains("Daglig Fast skal have en værdi på en af den pågældende dage"));
         }
         assertTrue(!patient.getOrdinationer().contains(dagligFast));
     }
+
     @Test
     void DagligFastUgyldig_TC2() {
 
         DagligFast dagligFast = null;
         try {
             dagligFast = controller.opretDagligFastOrdination(LocalDate.of(2024, 12, 12), LocalDate.of(2024, 12, 24), patient, laegemiddel, -1, 1, 2, 0);
-        }
-        catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             assertTrue(e.getMessage().contains("Ordinationerne må ikke være negative"));
         }
         assertTrue(!patient.getOrdinationer().contains(dagligFast));
@@ -129,7 +125,7 @@ class ControllerTest {
                     laegemiddel,
                     new LocalTime[]{LocalTime.of(11, 30), LocalTime.of(17, 30), LocalTime.of(23, 30)},
                     new double[]{2, 5, 2});
-        } catch (Exception exception){
+        } catch (Exception exception) {
             assertTrue(exception.getMessage().contains("Start dato skal være før slut dato!"));
         }
 
@@ -141,7 +137,7 @@ class ControllerTest {
                     laegemiddel,
                     new LocalTime[]{LocalTime.of(11, 30), LocalTime.of(17, 30)},
                     new double[]{2, 5, 2});
-        } catch (Exception exception){
+        } catch (Exception exception) {
             assertTrue(exception.getMessage().contains("Det skal være én dosis for hvert klokkeslet"));
         }
     }
